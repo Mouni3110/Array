67e7782ebc033c864d1b5655e304c7969fe2ef94
Mounika <mounikagurram366@gmail.com>
2023-08-04 16:27:21 +0530

    Array

---
+++ intial.java
@@ -0,0 +1,16 @@
+class Value{
+       public static void main(String[]args){
+         int a=123;
+         float b=12.3f;
+         double c=23.4;
+         System.out.println(a);
+         System.out.println(b);
+         System.out.println(c);
+         int[] arr={1,2,3,4,5};
+         for(int i: arr){
+            System.out.println(i);
+         }
+
+
+    }
+}

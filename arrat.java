4ef1ba9a00114e7d4201af129479c76e75b8f25a
Mounika <mounikagurram366@gmail.com>
2023-08-09 10:18:14 +0530

    array

---
+++ arrat.java
@@ -0,0 +1,8 @@
+class Test{
+    static public void main(String[] mouni){
+        int[] salarys={35000,33000,45000,5500};
+        for(int salary:salarys){
+        System.out.println(salary);
+    }
+}
+}

1c19a3544f7995324d8365317c471e879e98192a
Mounika <mounikagurram366@gmail.com>
2023-08-14 10:24:31 +0530

    pack

---
+++ pack/aa.java
@@ -0,0 +1,6 @@
+package mouni;
+public class aa{
+    public void m7(){
+        System.out.println("m7 is public ");
+    }
+}
---
+++ pack/demo.java
@@ -0,0 +1,10 @@
+package mouni;
+import mounika.a;
+public class demo{
+    public static void main(String[] agrs){
+       aa A=new aa();
+        A.m7();
+        a B=new a();
+        B.m1();
+    }
+}
